dee7d5ae11a4ee8a7201a04893f73cf978623f2e
Fjodor van Slooten <f.vanslooten@utwente.nl>
2025-09-19 17:00:50 +0200

    Create dict_to_json.py

---
+++ Datascience/dict_to_json_file/dict_to_json.py
@@ -0,0 +1,16 @@
+# https://www.w3schools.com/python/python_json.asp
+
+import json
+
+# a Python object (dict):
+person_data = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+json_string = json.dumps(person_data)
+
+# the result is a JSON string:
+print(json_string)
