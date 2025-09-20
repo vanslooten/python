230f5155a74e486bd249944e449ab2619c49aac2
Fjodor van Slooten <f.vanslooten@utwente.nl>
2025-09-20 15:16:53 +0200

    Create 1_open_dataset.py

---
+++ Datascience/first_dataset/1_open_dataset.py
@@ -0,0 +1,32 @@
+# open a CSV dataset using pandas
+# perform basic data exploration
+import pandas as pd
+
+# dataset source https://www.kaggle.com/datasets/burnoutminer/heights-and-weights-dataset/data
+# read csv file that contains a dataset
+df = pd.read_csv('SOCR-HeightWeight.csv')
+
+# what is the type of df?
+print(type(df))
+
+# what are the columns and
+# what do the first few rows look like?
+print(df.head())
+
+def convert_to_cm(height_inches):
+    """
+    Convert height from inches to centimeters.
+    Parameter:
+        height_inches (float): Height in inches.
+    Returns:
+        float: Height in centimeters.
+    """
+    return height_inches * 2.54 
+
+# lets investigate some information about the Height
+print("Max:", convert_to_cm(df['Height(Inches)'].max()) )
+print("Min:", convert_to_cm(df['Height(Inches)'].min()) )
+print("Mean:", convert_to_cm(df['Height(Inches)'].mean()) )
+print("Median:", convert_to_cm(df['Height(Inches)'].median()) )
+print("Std Dev:", convert_to_cm(df['Height(Inches)'].std()) )
+print("Variance:", convert_to_cm(df['Height(Inches)'].var()) )
